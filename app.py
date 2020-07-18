d58ca8035bcac62b4b9180fe83c50dd8c1bbb360
Dmitry Chernyshev <chernyshevdv@gmail.com>
2020-07-18 12:02:26 +0300

    Part 1 complete

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, make_response, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/<page_name>')
+def other_page(page_name):
+    response = make_response(render_template('404.html'), 404)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
